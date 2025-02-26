7032cc1352cb3e82ee0f2028cc34ba375b058d0c
Alberto <albertoromeropino2004@gmail.com>
2025-02-26 13:32:43 +0100

    Tutorial terminado

--- src/app/services/firebase.service.ts
+++ src/app/services/firebase.service.ts
@@ -1,44 +1,46 @@
-import { Injectable, inject } from '@angular/core';
-import {
-  Auth,
-  createUserWithEmailAndPassword,
-  signInWithEmailAndPassword,
-  updateProfile,
-  UserCredential,
-  sendPasswordResetEmail,
-} from '@angular/fire/auth';
-import {
-  Storage,
-  uploadString,
-  ref,
-  getDownloadURL,
-} from '@angular/fire/storage';
+import { Directive, inject, Injectable } from '@angular/core';
+import { Auth, createUserWithEmailAndPassword, sendPasswordResetEmail, signInWithEmailAndPassword, updateProfile, UserCredential } from '@angular/fire/auth';
 import { User } from '../models/user.model';
-import {
-  Firestore,
-  setDoc,
-  doc,
-  getDoc,
-  addDoc,
-  collection,
-  collectionData,
-  query,
-} from '@angular/fire/firestore';
+import { doc, Firestore, getDoc, setDoc, addDoc, collection, collectionData, query, updateDoc, deleteDoc, QueryConstraint, orderBy, limit } from '@angular/fire/firestore';
+import { UnsubscriptionError } from 'rxjs';
+import { deleteObject, uploadString } from '@firebase/storage';
+import { getDownloadURL, ref, Storage } from '@angular/fire/storage';
+import { QueryOptions } from './query-options.interface';
+
 
 @Injectable({
-  providedIn: 'root',
+  providedIn: 'root'
 })
+
 export class FirebaseService {
   auth = inject(Auth);
   firestore = inject(Firestore);
-  storage = inject(Storage);
+  storage = inject(Storage)
+
+  constructor() { }
+
 
   signIn(user: User): Promise<UserCredential> {
-    return signInWithEmailAndPassword(this.auth, user.email, user.password);
-  }
+    return signInWithEmailAndPassword(
+        this.auth,
+        user.email,
+        user.password
+      );
+    }
+
 
   signUp(user: User): Promise<UserCredential> {
-    return createUserWithEmailAndPassword(this.auth, user.email, user.password);
+    return createUserWithEmailAndPassword(
+      this.auth,
+      user.email,
+      user.password
+    );
+  }
+
+  async signOut() {
+    await this.auth.signOut();
+    localStorage.removeItem('user');
+    window.location.reload();
   }
 
   async updateUser(displayName: string) {
@@ -49,14 +51,53 @@ export class FirebaseService {
     }
   }
 
-  sendRecoveryEmail(email: string) {
-    return sendPasswordResetEmail(this.auth, email);
+  setDocument(path: string, data: any) {
+    return setDoc(doc(this.firestore, path), data)
   }
 
-  async signOut() {
-    await this.auth.signOut();
-    localStorage.removeItem('user');
-    window.location.reload();
+  addDocument(path: string, data: any) {
+    return addDoc(collection(this.firestore, path), data)
+  }
+
+  updateDocument(path: string, data: any) {
+    return updateDoc(doc(this.firestore, path), data)
+  }
+
+deleteDocument(path: string) {
+  return deleteDoc(doc(this.firestore, path));
+}
+
+  async getDocument(path: string) {
+    const docSnap = await getDoc(doc(this.firestore, path));
+    return docSnap.data();
+  }
+
+  buildQueryConstraints(options?: QueryOptions) : QueryConstraint[] {
+    const constraints: QueryConstraint[] = []
+    if (options?.orderBy) {
+      constraints.push(
+        orderBy(options?.orderBy.field, options?.orderBy.direction)
+      )
+    }
+
+    if (options?.limit) {
+      constraints.push(
+        limit(options?.limit)
+      )
+    }
+
+    return constraints;
+  }
+
+
+  async getCollectionData(path: string, collectionQuery? :QueryOptions) {
+    const ref = collection(this.firestore, path)
+    const constraints = this.buildQueryConstraints(collectionQuery);
+    return collectionData(query(ref, ...constraints), {idField: 'id'})
+  }
+
+  sendRecoveryEmail(email: string) {
+    return sendPasswordResetEmail(this.auth, email);
   }
 
   async isAuthenticated() {
@@ -64,37 +105,26 @@ export class FirebaseService {
       const unsubscribe = this.auth.onAuthStateChanged((user) => {
         unsubscribe();
         if (user) {
-          resolve(true);
+          resolve (true);
         } else {
-          resolve(false);
+          resolve (false);
         }
-      });
-    });
+      })
+    })
     return userExists;
   }
 
-  async getDocument(path: string) {
-    const docSnap = await getDoc(doc(this.firestore, path));
-    return docSnap.data();
+  async uploadImage(path: string, imageDataUrl: string) {
+    return uploadString(ref(this.storage, path), imageDataUrl, 'data_url').then( () => {
+      return getDownloadURL(ref(this.storage, path))
+    })
   }
 
-  setDocument(path: string, data: any) {
-    return setDoc(doc(this.firestore, path), data);
-  }
-  addDocument(path: string, data: any) {
-    return addDoc(collection(this.firestore, path), data);
+  async getFilePath(url: string) {
+    return ref(this.storage, url).fullPath
   }
 
-  getCollectionData(path: string, collectionQuery?: any) {
-    const ref = collection(this.firestore, path);
-    return collectionData(query(ref, collectionQuery), { idField: 'id' });
-  }
-
-  async uploadImage(path: string, imageUrl: string) {
-    return uploadString(ref(this.storage, path), imageUrl, 'data_url').then(
-      () => {
-        return getDownloadURL(ref(this.storage, path));
-      }
-    );
+  async deleteFile(path: string) {
+    return deleteObject(ref(this.storage, path))
   }
 }
--- src/app/shared/components/add-update-miniature/add-update-miniature.component.ts
+++ src/app/shared/components/add-update-miniature/add-update-miniature.component.ts
@@ -1,4 +1,4 @@
-import { Component, inject, OnInit } from '@angular/core';
+import { Component, inject, Input, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {
   FormControl,
@@ -29,6 +29,17 @@ import { IonButton, IonAvatar } from '@ionic/angular/standalone';
 import { FirebaseService } from 'src/app/services/firebase.service';
 import { User } from 'src/app/models/user.model';
 import { UtilsService } from 'src/app/services/utils.service';
+import { Miniature } from 'src/app/models/miniature.model';
+
+interface MiniatureForm {
+  id: string | null;
+  image: string | null;
+  name: string | null;
+  hp: number | null;
+  hit: number | null;
+  minerals: number | null;
+  building: string | null;
+}
 
 @Component({
   selector: 'app-add-update-miniature',
@@ -47,6 +58,7 @@ import { UtilsService } from 'src/app/services/utils.service';
   ],
 })
 export class AddUpdateMiniatureComponent implements OnInit {
+  @Input() miniature: MiniatureForm | null = null;
   firebaseService = inject(FirebaseService);
   utilsService = inject(UtilsService);
 
@@ -56,8 +68,10 @@ export class AddUpdateMiniatureComponent implements OnInit {
     id: new FormControl(''),
     image: new FormControl('', [Validators.required]),
     name: new FormControl('', [Validators.required, Validators.minLength(4)]),
-    units: new FormControl('', [Validators.required, Validators.min(1)]),
-    strength: new FormControl('', [Validators.required, Validators.min(0)]),
+    hp: new FormControl(0, [Validators.required, Validators.min(1)]),
+    hit: new FormControl(0, [Validators.required, Validators.min(1)]),
+    minerals: new FormControl(0, [Validators.required, Validators.min(1)]),
+    building: new FormControl('', [Validators.required]),
   });
 
   constructor() {
@@ -71,8 +85,12 @@ export class AddUpdateMiniatureComponent implements OnInit {
       checkmarkCircleOutline,
     });
   }
+
   ngOnInit() {
     this.user = this.utilsService.getFromLocalStorage('user');
+    if (this.miniature) {
+      this.form.setValue(this.miniature);
+    }
   }
 
   async takeImage() {
@@ -86,43 +104,97 @@ export class AddUpdateMiniatureComponent implements OnInit {
 
   async submit() {
     if (this.form.valid) {
-      const loading = await this.utilsService.loading();
-      await loading.present();
+      if (this.miniature) {
+        this.updateMiniature();
+      } else {
+        this.createMiniature();
+      }
+    }
+  }
 
-      const path: string = `users/${this.user.uid}/miniatures`;
+  async createMiniature() {
+    const loading = await this.utilsService.loading();
+    await loading.present();
+
+    const path: string = `users/${this.user.uid}/miniatures`;
+    const imageDataUrl = this.form.value.image;
+    const imagePath = `${this.user.uid}/${Date.now()}`;
+    const imageUrl = await this.firebaseService.uploadImage(
+      imagePath,
+      imageDataUrl!
+    );
+    this.form.controls.image.setValue(imageUrl);
+    delete this.form.value.id;
+
+    this.firebaseService
+      .addDocument(path, this.form.value)
+      .then(async (res) => {
+        this.utilsService.dismissModal({ success: true });
+        this.utilsService.presentToast({
+          message: 'Miniatura añadida exitosamente',
+          duration: 1500,
+          color: 'success',
+          position: 'middle',
+          icon: 'checkmark-circle-outline',
+        });
+      })
+      .catch((error) => {
+        this.utilsService.presentToast({
+          message: error.message,
+          duration: 2500,
+          color: 'danger',
+          position: 'middle',
+          icon: 'alert-circle-outline',
+        });
+      })
+      .finally(() => {
+        loading.dismiss();
+      });
+  }
+
+  async updateMiniature() {
+    const loading = await this.utilsService.loading();
+    await loading.present();
+
+    const path: string = `users/${this.user.uid}/miniatures/${this.miniature!.id}`;
+    if (this.form.value.image != this.miniature!.image) {
       const imageDataUrl = this.form.value.image;
+      const oldImagePath = await this.firebaseService.getFilePath(
+        this.miniature!.image!
+      );
+      await this.firebaseService.deleteDocument(oldImagePath);
       const imagePath = `${this.user.uid}/${Date.now()}`;
       const imageUrl = await this.firebaseService.uploadImage(
         imagePath,
         imageDataUrl!
       );
       this.form.controls.image.setValue(imageUrl);
-      delete this.form.value.id;
-
-      this.firebaseService
-        .addDocument(path, this.form.value)
-        .then(async (res) => {
-          this.utilsService.dismissModal({ success: true });
-          this.utilsService.presentToast({
-            message: 'Mininatura añadida exitosamente',
-            duration: 1500,
-            color: 'success',
-            position: 'middle',
-            icon: 'checkmark-circle-outline',
-          });
-        })
-        .catch((error) => {
-          this.utilsService.presentToast({
-            message: error.message,
-            duration: 2500,
-            color: 'danger',
-            position: 'middle',
-            icon: 'alert-circle-outline',
-          });
-        })
-        .finally(() => {
-          loading.dismiss();
-        });
     }
+    delete this.form.value.id;
+
+    this.firebaseService
+      .updateDocument(path, this.form.value)
+      .then(async (res) => {
+        this.utilsService.dismissModal({ success: true });
+        this.utilsService.presentToast({
+          message: 'Miniatura editada exitosamente',
+          duration: 1500,
+          color: 'success',
+          position: 'middle',
+          icon: 'checkmark-circle-outline',
+        });
+      })
+      .catch((error) => {
+        this.utilsService.presentToast({
+          message: error.message,
+          duration: 2500,
+          color: 'danger',
+          position: 'middle',
+          icon: 'alert-circle-outline',
+        });
+      })
+      .finally(() => {
+        loading.dismiss();
+      });
   }
 }
